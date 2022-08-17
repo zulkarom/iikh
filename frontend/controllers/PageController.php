aab98e1e0ea4276b4d26d61e104ad664762a1e73
zulkarom <zulkarom@gmail.com>
2022-08-17 11:56:09 +0800

    w

--- frontend/controllers/PageController.php
+++ frontend/controllers/PageController.php
@@ -4,30 +4,11 @@ namespace frontend\controllers;
 
 use Yii;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
-use yii\db\Expression;
-use backend\models\Order;
-use backend\models\OrderItem;
-use backend\models\OrderAddress;
-use backend\models\Product;
-use backend\modules\shop\models\ShippingRate;
 /**
  * BizCanvasController implements the CRUD actions for BizCanvas model.
  */
 class PageController extends Controller
 {   
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
 
 
     public function actionAbout()
