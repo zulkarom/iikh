dfb26baba846bb2774f07927737e18400d226d94
zulkarom <zulkarom@gmail.com>
2022-08-19 00:02:19 +0800

    p

--- backend/models/billplz/Connect.php
+++ backend/models/billplz/Connect.php
@@ -310,6 +310,7 @@ class Connect
         $data['paid'] = $data['paid'] === 'true' ? true : false;
 
         $signed = hash_hmac('sha256', $signing, $x_signature_key);
+        print_r($_GET['billplz']);
         echo 'yoo===' .$signed . '====' . $data['x_signature'];die();
 
         if (hash_equals($signed, $data['x_signature'])) {
