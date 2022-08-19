f7b2feb124e6bbb02c6582c9b9db15faf03bb3bd
zulkarom <zulkarom@gmail.com>
2022-08-19 10:45:33 +0800

    try

--- backend/models/billplz/Connect.php
+++ backend/models/billplz/Connect.php
@@ -312,8 +312,8 @@ class Connect
         $data['paid'] = $data['paid'] === 'true' ? true : false;
 
         $signed = hash_hmac('sha256', $signing, $x_signature_key);
-        print_r(\Yii::$app->request->post());
-       echo 'jutawan===' .$signed . '====' . $data['x_signature'];die(); 
+       // print_r(\Yii::$app->request->post());
+      // echo 'jutawan===' .$signed . '====' . $data['x_signature'];die(); 
 
         if (hash_equals($signed, $data['x_signature'])) {
             $data['type'] = $type;
